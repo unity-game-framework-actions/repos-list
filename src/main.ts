93a71dbb3f16c60a26de86e98ba97c5cc3b6050b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-08-27 23:02:21 +0300

    Add action

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ run()
 async function run(): Promise<void> {
   try {
     const user = core.getInput('user', {required: true})
+    const visibility = core.getInput('visibility', {required: true})
     const config = await utility.readConfigAny()
     const context = await utility.getContextAny()
-    const result = await action.createList(user, config, context)
+    const result = await action.createList(user, visibility, config, context)
 
     await utility.setOutput(result)
   } catch (error) {
